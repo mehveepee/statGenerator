8bac67e73325bbe07f7c7ab5a99f6125f18dda62
mehveepee <mehveepee@gmail.com>
2021-07-25 22:35:43 -0400

    first roll

--- statRoller.jsx
+++ statRoller.jsx
@@ -1,4 +1,36 @@
-let roller = () => {
-    Math.floor(Math.random()*20)
-};
-console.log(roller);
+function roller(max) {
+    return Math.floor(Math.random()*max + 1);
+}
+
+const rollOne = () => {
+    let dOne = roller(20);
+        console.log(dOne);
+    let dTwo = roller(20);
+        console.log(dTwo);
+    let dThree = roller(20);
+        console.log(dThree);
+    let dFour = roller(20);
+        console.log(dFour);
+    let dFive = roller(20);
+        console.log(dFive);
+    let dSix = roller(20);
+        console.log(dSix);
+    const setOne = [
+        dOne,
+        dTwo,
+        dThree,
+        dFour,
+        dFive,
+        dSix
+    ]
+    console.log(setOne);
+    var sumOne = setOne.reduce(function(a, b){
+        return a + b;
+    }, 0);
+console.log(sumOne);        
+    }
+function combine(a) {
+    use_sumOne(a);
+}
+rollOne();
+combine();
